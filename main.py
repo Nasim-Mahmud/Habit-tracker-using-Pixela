42e83d5489e7bacb27dca2c1f089f65d62d4973b
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-29 13:01:00 +0600

    Implementing graph parameters.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 
-
 USER_NAME = "nas13"
 TOKEN = "ryC7YTsmnDf7Bc57"
 pixela_endpoint = "https://pixe.la/v1/users"
@@ -12,16 +11,16 @@ parameters = {
     "notMinor": "yes"
 }
 
-response = requests.post(url=pixela_endpoint, json=parameters)
-print(response.text)
+# response = requests.post(url=pixela_endpoint, json=parameters)
+# print(response.text)
 
 graph_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs"
 
-graph_parameters= {
-    "id":,
-    "name":,
-    "unit":,
-    "type":,
-    "color":,
+graph_parameters = {
+    "id": "Graph",
+    "name": "Studying Graph",
+    "unit": "Hour",
+    "type": "float",
+    "color": "shibafu",
 }
 requests.post()
