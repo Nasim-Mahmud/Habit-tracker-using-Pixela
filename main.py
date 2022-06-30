99e5af1dd007e4071ceaa80a0350898fb80172f5
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-30 20:49:05 +0600

    Implementing request header.

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ parameters = {
 # response = requests.post(url=pixela_endpoint, json=parameters)
 # print(response.text)
 
+#Generating graph
 graph_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs"
 
 graph_parameters = {
