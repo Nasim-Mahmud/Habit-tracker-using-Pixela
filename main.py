9ab5498916a95c64738fdf24d3b9db68cdd8810b
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-07-01 06:17:08 +0600

    Creating a pixel into the graph.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 USER_NAME = "nas13"
 TOKEN = "ryC7YTsmnDf7Bc57"
 pixela_endpoint = "https://pixe.la/v1/users"
+GRAPH_ID = "graph01"
 
 parameters = {
     "token": TOKEN,
@@ -18,7 +19,7 @@ parameters = {
 graph_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs"
 
 graph_parameters = {
-    "id": "graph01",
+    "id": GRAPH_ID,
     "name": "Studying Graph",
     "unit": "Hour",
     "type": "float",
@@ -28,16 +29,16 @@ graph_parameters = {
 secure_headers = {
     "X-USER-TOKEN": TOKEN
 }
-response = requests.post(url=graph_endpoint, json=graph_parameters, headers=secure_headers)
-print(response.text)
+# response = requests.post(url=graph_endpoint, json=graph_parameters, headers=secure_headers)
+# print(response.text)
 
 # Generating a pixel
-pixel_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs/graph01"
+pixel_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs/{GRAPH_ID}"
 
 pixel_parameters = {
     "date": "20220701",
-    "quantity": "10",
+    "quantity": "10.0",
 
 }
-response = requests.get(url=pixel_endpoint, json=pixel_parameters, headers=secure_headers)
+response = requests.post(url=pixel_endpoint, json=pixel_parameters, headers=secure_headers)
 print(response.text)
